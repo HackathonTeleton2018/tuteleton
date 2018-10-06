5ec1e5b16de816afd35a19abf97779663c7099e8
ericking97 <ericking97@hotmail.com>
2018-10-06 08:31:26 -0500

    Finish endpoint carga on the admin

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -17,19 +18,39 @@ app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+var multipart = bodyParser.urlencoded({
+  extended: true,
+  parameterLimit: 100000,
+  limit: '50mb'
+});
+
+app.use(function(req, res, next) {
+  var isMultipart = /^multipart\//i;
+  var type = req.get('Content-Type');
+  if (isMultipart.test(type)) return multipart(req, res, next);
+  else return next();
+});
+
+app.use(bodyParser.json({
+  parameterLimit: 100000,
+  limit: '50mb'
+}));
 
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- controllers/api/CargaController.js
+++ controllers/api/CargaController.js
@@ -5,7 +5,12 @@ const SOURCE = 'tuteleton:controllers:api:CargaController';
 const debug = require('debug')(SOURCE);
 
 module.exports = {
+    index: (req, res, next) => {
+        return res.render('admin/carga');
+    },
     carga: (req, res, next) => {
+        console.log(req.body);
+        console.log(req.file);
         // File is on req.file
     }
 };
