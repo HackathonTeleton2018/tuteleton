bd7d9b162db2120d5f2a78ba4d813411ed4336f7
ericking97 <ericking97@hotmail.com>
2018-10-06 00:28:38 -0500

    Add consolidation table

---
+++ migrations/20181006045018-create-table-consolidacion.js
@@ -0,0 +1,56 @@
+/* jshint node: true */
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('consolidacion', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      origen: {
+        type: Sequelize.ENUM('Banamex', 'Farmacias del Ahorro', 'Soriana', 'Telcel', 'Telecomm', 'Infinitum', 'Telmex'),
+        allowNull: false
+      },
+      donacionUnitaria: {
+        fieldName: 'donacion_unitaria',
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+      },
+      donacionMultiple: {
+        fieldName: 'donacion_multiple',
+        type: Sequelize.INTEGER,
+        allowNull: true
+      },
+      fecha: {
+        type: Sequelize.DATEONLY,
+        allowNull: true
+      },
+      donativo: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+      },
+      estado: {
+        type: Sequelize.TEXT,
+        allowNull: true
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+        allowNull: false,
+        field: 'created_at'
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+        allowNull: false,
+        field: 'updated_at'
+      }
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('consolidacion');
+  }
+};
